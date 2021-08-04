70863c42534d1856ce92179b4838d58efbbc6bc9
Seth-Ese <sethkameta@gmail.com>
2021-08-04 11:21:14 +0100

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const FriendList = require("./Model/FriendList");
+let FriendList = require("./Model/FriendList");
 const app = express();
 
 app.use(express.urlencoded({extended:true}))
@@ -29,7 +29,7 @@ app.get('/',(req,res)=>{
             let newFriends = req.body
             let friends = {
 
-                id: newFriends.id,
+                id: FriendList.length + 1,
                 name: newFriends.name,
                 Age: newFriends.Age,
                 phone: newFriends.phone
@@ -39,8 +39,35 @@ app.get('/',(req,res)=>{
             res.json(FriendList)
         })
 
-        
+      // Updating
+      
+      app.put('/:id', (req,res)=>{
+          const {id} = req.params
+          const body = req.body
 
+          const newUpdates = FriendList.find((list)=> list.id === Number(id))
+          const index = FriendList.indexOf(newUpdates)
+
+        if(!newUpdates){
+            res.status(404).send("Cannot be Updated")
+        }
+        const updated = {...newUpdates, ...body}
+        FriendList[index]= updated
+        res.status(200).send(updated)
+      })
+        // Delete
+      app.delete('/:id',(req,res)=>{
+          const {id} = req.params
+
+          const deleteFriend = FriendList.filter((friend)=> friend.id != Number(id))
+
+          if(!deleteFriend){
+              res.status(404).send('Friend not found')
+          }
+          FriendList = deleteFriend
+          res.send(FriendList)
+      })
+    
 
 app.listen(5000,()=>{
     console.log('sever is running at this port...');
